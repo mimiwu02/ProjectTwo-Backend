68540f3967bfafe1821c78b69a69eaae38ff419a
Mimi Wu <im.mimiwu@gmail.com>
2016-07-22 13:10:00 -0400

    changed mongoUrl

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ var MongoClient = mongodb.MongoClient;
 
 /* Connection url where your mongodb server is running. */
 //var mongoUrl = 'mongodb://localhost:27017/notes';
-var mongoUrl = 'mongodb://heroku_8npb49rs:60kb260mr8jj6k1t94149u13ts@ds027175.mlab.com:27175/heroku_8npb49rs/notes'
+var mongoUrl = 'mongodb://heroku_8npb49rs:60kb260mr8jj6k1t94149u13ts@ds027175.mlab.com:27175/heroku_8npb49rs'
 
 
 /* welcome page */
